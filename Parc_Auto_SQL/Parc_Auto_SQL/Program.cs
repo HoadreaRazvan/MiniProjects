0d0b5c248ff536e0781ac37259bd6e30af76f752
HoadreaRazvan <162281824+HoadreaRazvan@users.noreply.github.com>
2024-05-05 12:14:31 +0300

    Parc_Auto_SQL

---
+++ Parc_Auto_SQL/Parc_Auto_SQL/Program.cs
@@ -0,0 +1,32 @@
+﻿using App.services;
+using System;
+
+using App.exceptions;
+
+namespace Parc_Auto_SQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MasinaService service = new MasinaService();
+            //int y = 0;
+            //try
+            //{
+            //    //service.create(new App.Masina("Dacia", "Logan", 321, 47));
+            //    int x = 7 / y;
+
+            //}catch(MasinaExistsException e)
+            //{
+            //    Console.WriteLine("atentie");       
+            //}
+
+            //service.create(new App.Masina("1", "1", 1, 1));
+
+            //service.deleteByMarca("1");
+            //service.deleteById(123);
+
+
+        }
+    }
+}
